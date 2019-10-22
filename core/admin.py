e50c72d2b2b2b68839e0e3a9aa2007d301356991
girishagoel <girishagoel@gmail.com>
2019-10-22 17:28:04 +0100

    new cart module added

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book, Author
+from .models import Book, Author,Cart
 
 #admin.site.register(Book)
 @admin.register(Book)
@@ -7,3 +7,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('name','author','price','published_date')
 
 admin.site.register(Author)
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user','book','quantity','total_price']
+    
--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 class Author(models.Model):
     author_name = models.CharField(max_length = 25)
@@ -17,8 +18,14 @@ class Book(models.Model):
     description = models.TextField(max_length= 250, blank= True, null=True)
     image = models.ImageField(upload_to = 'images/',blank = True, null = True)
 
-
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, null = True, blank = True)
+    book = models.ForeignKey(Book, on_delete = models.CASCADE, null = True, blank = True)
+    quantity = models.IntegerField(null = True, blank = True)
+    total_price = models.IntegerField(null = True, blank = True)
 #  def __str__(self):
 #     return self.name + '    ' + self.author + '      ' + str(self.published_date)
-    
+    # def ttprice(self):
+    #     self.total_price = self.book.price * quantity
+    #     return self.total_price
 
